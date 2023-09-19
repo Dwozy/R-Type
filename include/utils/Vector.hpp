3c8c9d64aef43d323f881dcec82901f599e6ec7c
Leo Soidiki <leo.soidiki@epitech.eu>
2023-09-19 11:02:26 +0200

    feat(utils/vector) add function for vector

--- include/utils/Vector.hpp
+++ include/utils/Vector.hpp
@@ -7,25 +7,47 @@
 
 #ifndef VECTOR_HPP_
     #define VECTOR_HPP_
-
-class Vector2Int
-{
-    public:
-        Vector2Int(int x, int y): x(x), y(y) {};
-        ~Vector2Int() = default;
-
-        int x;
-        int y;
-};
+    #include <cmath>
 
 class Vector2
 {
     public:
         Vector2(float x, float y): x(x), y(y) {};
         ~Vector2() = default;
-
+        float length()
+        {
+            return (sqrt(pow(x, 2)+ pow(y, 2)));
+        }
+        Vector2 normalize()
+        {
+            return Vector2(x / length(), y / length());
+        }
+        float dotProduct(Vector2 vec)
+        {
+            return (x * vec.x + y * vec.y);
+        }
         float x;
         float y;
+};
+
+class Vector2Int
+{
+    public:
+        Vector2Int(int x, int y): x(x), y(y) {};
+        ~Vector2Int() = default;
+        float length()
+        {
+            return (sqrt(pow(x, 2)+ pow(y, 2)));
+        }
+        Vector2 normalize() {
+            return Vector2(x / length(), y / length());
+        }
+        float dotProduct(Vector2 vec)
+        {
+            return (x * vec.x + y * vec.y);
+        }
+        int x;
+        int y;
 };
 
 #endif /* !VECTOR_HPP_ */
