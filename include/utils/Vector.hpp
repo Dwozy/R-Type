33b401cbe7deaa9a062bdf56051017b0f996bd63
Leo Soidiki <leo.soidiki@epitech.eu>
2023-09-19 14:01:09 +0200

    feat(utils/vector): add Vector in namespace GameEngine

--- include/utils/Vector.hpp
+++ include/utils/Vector.hpp
@@ -9,35 +9,36 @@
     #define VECTOR_HPP_
     #include <cmath>
 
-template <typename T>
-class Vector2
-{
-    public:
-        Vector2(T x, T y): x(x), y(y) {};
-        ~Vector2() = default;
-        float length()
-        {
-            return (sqrt(pow(x, 2)+ pow(y, 2)));
-        }
-        Vector2 normalize()
-        {
-            return Vector2(x / length(), y / length());
-        }
-        float dotProduct(Vector2 vec)
-        {
-            return (x * vec.x + y * vec.y);
-        }
-        Vector2 operator+(const Vector2 &vec) {
-            return (Vector2(x + vec.x, y + vec.y));
-        }
-        Vector operator-(const Vector2 &vec) {
-            return (Vector2(x + (-vec.x), y + (-vec.y)));
-        }
-        T operator*(const Vector2 &vec) {
-            return (y * vec.y + x * vec.x);
-        }
-        T x;
-        T y;
-};
-
+namespace GameEngine {
+    template <typename T>
+    class Vector2
+    {
+        public:
+            Vector2(T x, T y): x(x), y(y) {};
+            ~Vector2() = default;
+            float length()
+            {
+                return (sqrt(pow(x, 2)+ pow(y, 2)));
+            }
+            Vector2 normalize()
+            {
+                return Vector2(x / length(), y / length());
+            }
+            float dotProduct(Vector2 vec)
+            {
+                return (x * vec.x + y * vec.y);
+            }
+            Vector2 operator+(const Vector2 &vec) {
+                return (Vector2(x + vec.x, y + vec.y));
+            }
+            Vector operator-(const Vector2 &vec) {
+                return (Vector2(x + (-vec.x), y + (-vec.y)));
+            }
+            T operator*(const Vector2 &vec) {
+                return (y * vec.y + x * vec.x);
+            }
+            T x;
+            T y;
+    };
+}
 #endif /* !VECTOR_HPP_ */
