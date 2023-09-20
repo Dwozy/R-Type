c2f14ef51a88877f3d68c1e51fa2c250e2634953
BuildTools <unconfigured@null.spigotmc.org>
2023-09-20 13:43:38 +0200

    fix(engine): build_win script now delete created lib

--- build_win.bat
+++ build_win.bat
@@ -21,14 +21,13 @@ cmake --build .\build --config Release --clean-first
 goto:eof
 
 :fclean
-
 echo -- Cleaning build folder
-if exist ".\build" rmdir ".\build" /q
+if exist ".\build" rmdir ".\build" /q /s
 echo -- Cleaning build folder - done
 echo -- Cleaning GameEngine folder
-if exist ".\GameEngine" rmdir ".\GameEngine" /q
+if exist ".\GameEngine" rmdir ".\GameEngine" /q /s
 echo -- Cleaning GameEngine folder - done
 echo -- Cleaning libGameEngine.lib
-if exist ".\libGameEngine.lib" del ".\libGameEngine.lib" /q
+if exist ".\GameEngine.lib" del ".\GameEngine.lib"
 echo -- Cleaning libGameEngine.lib - done
 goto:eof
