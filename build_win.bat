1376718e3a971fb27f8d0ce498d07c185136a1eb
Aldric <aldric.jourdain@epitech.eu>
2023-09-20 12:16:22 +0200

    fix(engine): change build scripts command 'rebuild' to 're' and 'clean' to 'fclean'

--- build_win.bat
+++ build_win.bat
@@ -1,12 +1,12 @@
 @echo off
 
-if "%~1"=="clean" (
-    call:clean
+if "%~1"=="fclean" (
+    call:fclean
     exit /b
 )
 
-if "%~1"=="rebuild" (
-    call:clean
+if "%~1"=="re" (
+    call:fclean
     call:build
     exit /b
 )
@@ -20,8 +20,15 @@ cmake -S . -B build
 cmake --build .\build --config Release --clean-first
 goto:eof
 
-:clean
+:fclean
+
 echo -- Cleaning build folder
-if exist ".\build" rmdir ".\build" /q /s
+if exist ".\build" rmdir ".\build" /q
 echo -- Cleaning build folder - done
+echo -- Cleaning GameEngine folder
+if exist ".\GameEngine" rmdir ".\GameEngine" /q
+echo -- Cleaning GameEngine folder - done
+echo -- Cleaning libGameEngine.lib
+if exist ".\libGameEngine.lib" del ".\libGameEngine.lib" /q
+echo -- Cleaning libGameEngine.lib - done
 goto:eof
