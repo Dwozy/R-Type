bc5bbe256151615d00b85edfa9835aee54d1473b
Axel Humeau <axel.humeau@epitech.eu>
2023-09-26 19:45:41 +0200

    feat: setup conan for windows

--- GameEngine/build_win.bat
+++ GameEngine/build_win.bat
@@ -16,7 +16,8 @@ call:build
 exit /b
 
 :build
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=..\submodules\vcpkg\scripts\buildsystems\vcpkg.cmake
+conan install . --output-folder=build --build=missing -c tools.system.package_manager:mode=install
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=build/build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .\build --config Release --clean-first
 goto:eof
 
