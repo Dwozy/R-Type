c8646abd5593e8adae7fde65ba9d19ac9ba265a2
AxelHumeau <axel.humeau@epitech.eu>
2023-09-28 12:16:19 +0200

    fix: windows compilation error related to conan generator

--- GameEngine/build_win.bat
+++ GameEngine/build_win.bat
@@ -17,7 +17,7 @@ exit /b
 
 :build
 set CONAN_BUILD_DIR=build\build\generators
-conan install . --output-folder=build --build=missing -c tools.system.package_manager:mode=install -g visual_studio
+conan install . --output-folder=build --build=missing -c tools.system.package_manager:mode=install -g MSBuildDeps
 cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=%CONAN_BUILD_DIR%\conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .\build --config Release --clean-first
 goto:eof
