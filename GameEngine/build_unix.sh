1c4998124bb4d4198f48d97b5c008f30e1b8d7ff
Axel Humeau <axel.humeau@epitech.eu>
2023-10-08 17:31:58 +0200

    build: use build=missing to install dependencies with conan

--- GameEngine/build_unix.sh
+++ GameEngine/build_unix.sh
@@ -5,26 +5,11 @@ build()
     export CMAKE_MAKE_PROGRAM=$(which make)
     export CMAKE_CXX_COMPILER=$(which g++)
     export INCLUDES_DIR=$(pwd)/include
-    conan build . --output-folder=.
+    conan build . --output-folder=. --build=missing
     status=$?
     if [ "$status" -ne 0 ]; then
         exit "$status"
     fi
-    # ar rcT GameEngine/lib/libGameEngineTMP.a GameEngine/lib/libGameEngine.a build/Release/libs/*.a
-    # status=$?
-    # if [ "$status" -ne 0 ]; then
-    #     exit "$status"
-    # fi
-    # rm GameEngine/lib/libGameEngine.a
-    # status=$?
-    # if [ "$status" -ne 0 ]; then
-    #     exit "$status"
-    # fi
-    # mv GameEngine/lib/libGameEngineTMP.a GameEngine/lib/libGameEngine.a
-    # status=$?
-    # if [ "$status" -ne 0 ]; then
-    #     exit "$status"
-    # fi
 }
 
 fclean()
