6036f1ad6619f9fde72144978791a6964b746614
Axel Humeau <axel.humeau@epitech.eu>
2023-10-13 08:53:11 +0200

    chore: add execution permissions for build_artifact.sh

--- build_artifact.sh
+++ build_artifact.sh
@@ -8,7 +8,7 @@ mkdir -p ./artifacts/server
 cp -r R-Type/assets ./artifacts/client
 
 cp R-Type/r-type_client ./artifacts/client
-cp R-Type/r-type_server ./artifacts/server
+cp server/r-type_server ./artifacts/server
 
 zip artifacts/r-type_client_linux_x86-64.zip ./artifacts/client/*
 zip artifacts/r-type_server_linux_x86-64.zip ./artifacts/server/*
