73ba0ea9f175a721644d3cc3f311dbdff4f58c41
Axel Humeau <axel.humeau@epitech.eu>
2023-10-16 18:17:11 +0200

    feat: add installation script when build artifact

--- build_artifact.sh
+++ build_artifact.sh
@@ -10,5 +10,16 @@ cp -r R-Type/assets ./artifacts/client
 cp R-Type/r-type_client ./artifacts/client
 cp server/r-type_server ./artifacts/server
 
+echo '#!/bin/bash
+
+IFS="/" read -r -a array <<< "$SHELL"
+shell=${array[${#array[@]}-1]}
+
+echo "export PATH=\"$(pwd):$PATH\"" >> ~/."$shell"rc
+' > ./artifacts/server/install.sh
+
+chmod +x ./artifacts/server/install.sh
+cp ./artifacts/server/install.sh ./artifacts/client
+
 zip artifacts/r-type_client_linux_x86-64.zip ./artifacts/client/*
 zip artifacts/r-type_server_linux_x86-64.zip ./artifacts/server/*
