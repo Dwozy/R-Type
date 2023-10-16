a2902599bcea709d1f58f6aa3209d8862beba132
Axel Humeau <axel.humeau@epitech.eu>
2023-10-16 19:09:58 +0200

    build: add the shared library when build the release assets

--- build_artifact.sh
+++ build_artifact.sh
@@ -21,5 +21,8 @@ echo "export PATH=\"$(pwd):$PATH\"" >> ~/."$shell"rc
 chmod +x ./artifacts/server/install.sh
 cp ./artifacts/server/install.sh ./artifacts/client
 
+find . -name "*.so" -exec cp {} ./artifacts/client \;
+find . -name "*.so" -exec cp {} ./artifacts/server \;
+
 zip artifacts/r-type_client_linux_x86-64.zip ./artifacts/client/*
 zip artifacts/r-type_server_linux_x86-64.zip ./artifacts/server/*
