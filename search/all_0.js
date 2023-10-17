c106100f07b670205979bb19807a8d59b9affe80
AxelHumeau <AxelHumeau@users.noreply.github.com>
2023-10-17 12:22:45 +0000

    deploy: 318c76cacd638957a97901bab07ac613f1ad833d

---
+++ search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['r_2dtype_0',['R-Type',['../md_README.html',1,'']]],
+  ['readme_2emd_1',['README.md',['../README_8md.html',1,'']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['r_2dtype_3',['R-Type',['../md_README.html',1,'']]]
+];
