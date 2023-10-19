12ae8bf90d1ea76e5bc7a8fd5ed68e05edf29d07
Aldric <aldric.jourdain@epitech.eu>
2023-10-19 17:36:39 +0200

    feat: add basic tests for vectors

---
+++ GameEngine/tests/TestVector.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** TestVector
+*/
+
+#include <gtest/gtest.h>
+#include "utils/Vector.hpp"
+
+TEST(Vector, CreateIntVector){
+	EXPECT_NO_THROW(GameEngine::Vector2<int> vector(0, 0));
+}
+
+TEST(Vector, IntVectorNullLenght){
+	GameEngine::Vector2<int> vector(0, 0);
+	EXPECT_EQ(vector.length(), 0);
+}
+
+TEST(Vector, IntVectorLenght0_5){
+	GameEngine::Vector2<int> vector(0, 5);
+	EXPECT_EQ(vector.length(), 5.0f);
+}
+
+TEST(Vector, IntVectorLenght42_0){
+	GameEngine::Vector2<int> vector(42, 0);
+	EXPECT_EQ(vector.length(), 42.0f);
+}
