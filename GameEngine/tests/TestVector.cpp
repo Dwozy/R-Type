f03ed1e8ed2dc6966e839451581fa4f27839988e
BuildTools <unconfigured@null.spigotmc.org>
2023-10-20 11:54:19 +0200

    style: lint TestVector.cpp

--- GameEngine/tests/TestVector.cpp
+++ GameEngine/tests/TestVector.cpp
@@ -5,24 +5,28 @@
 ** TestVector
 */
 
-#include <gtest/gtest.h>
 #include "utils/Vector.hpp"
+#include <gtest/gtest.h>
 
-TEST(Vector, CreateIntVector){
-	EXPECT_NO_THROW(GameEngine::Vector2<int> vector(0, 0));
+TEST(Vector, CreateIntVector)
+{
+    EXPECT_NO_THROW(GameEngine::Vector2<int> vector(0, 0));
 }
 
-TEST(Vector, IntVectorNullLenght){
-	GameEngine::Vector2<int> vector(0, 0);
-	EXPECT_EQ(vector.length(), 0);
+TEST(Vector, IntVectorNullLenght)
+{
+    GameEngine::Vector2<int> vector(0, 0);
+    EXPECT_EQ(vector.length(), 0);
 }
 
-TEST(Vector, IntVectorLenght0_5){
-	GameEngine::Vector2<int> vector(0, 5);
-	EXPECT_EQ(vector.length(), 5.0f);
+TEST(Vector, IntVectorLenght0_5)
+{
+    GameEngine::Vector2<int> vector(0, 5);
+    EXPECT_EQ(vector.length(), 5.0f);
 }
 
-TEST(Vector, IntVectorLenght42_0){
-	GameEngine::Vector2<int> vector(42, 0);
-	EXPECT_EQ(vector.length(), 42.0f);
+TEST(Vector, IntVectorLenght42_0)
+{
+    GameEngine::Vector2<int> vector(42, 0);
+    EXPECT_EQ(vector.length(), 42.0f);
 }
