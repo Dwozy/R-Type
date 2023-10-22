2864015d0fde5fe9e6bae4e8db598c3e55ad44bb
Axel Humeau <axel.humeau@epitech.eu>
2023-10-22 20:20:16 +0200

    feat(GameEngine): create component for prefab

--- GameEngine/include/PrefabManager.hpp
+++ GameEngine/include/PrefabManager.hpp
@@ -11,20 +11,24 @@
 #include <string>
 #include <unordered_map>
 #include <typeindex>
+#include <functional>
 #include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 namespace GameEngine
 {
     class PrefabManager
     {
       public:
-        PrefabManager() = default;
+        PrefabManager();
         ~PrefabManager() = default;
         void loadPrefabFromFile(const std::string &filename);
 
       protected:
       private:
-        std::unordered_map<std::string, std::pair<std::type_index, std::any>> _prefabs;
+        std::unordered_map<std::string, std::unordered_map<std::type_index, std::any>> _prefabs;
+        std::unordered_map<std::string, std::function<std::pair<std::type_index, std::any> (json)>> _componentFunctions;
     };
 } // namespace GameEngine
 
--- GameEngine/src/PrefabManager.cpp
+++ GameEngine/src/PrefabManager.cpp
@@ -13,14 +13,23 @@
 
 namespace GameEngine
 {
+
+    PrefabManager::PrefabManager()
+    {
+        _componentFunctions["TransformComponent"] = [](json json){return std::pair(std::type_index(typeid(TransformComponent)), json.at("value").get<TransformComponent>());};
+        _componentFunctions["ControllableComponent"] = [](json json){return std::pair(std::type_index(typeid(ControllableComponent)), json.at("value").get<ControllableComponent>());};
+        _componentFunctions["TextureComponent"] = [](json json){return std::pair(std::type_index(typeid(TextureComponent)), json.at("value").get<TextureComponent>());};
+        _componentFunctions["CollisionComponent"] = [](json json){return std::pair(std::type_index(typeid(CollisionComponent)), json.at("value").get<CollisionComponent>());};
+    }
+
     void PrefabManager::loadPrefabFromFile(const std::string &filename)
     {
         std::ifstream infile(filename);
 
         if (!infile)
-            throw; // No file or invalid file
+            throw Error::InvalidPrefabFileError(); // No file or invalid file
 
-        nlohmann::json json = nlohmann::json::parse(infile);
+        json json = json::parse(infile);
         if (!json.contains("name"))
             throw Error::InvalidPrefabFileError();
         if (!json.contains("components"))
@@ -28,9 +37,11 @@ namespace GameEngine
         std::string name(json["name"].get<std::string>());
         if (_prefabs.contains(name))
             throw Error::PrefabNameAlreadyUsedError();
+
         for (auto &component: json["component"]) {
-            if (!component.contains("type"))
+            if (!component.contains("type") || !_componentFunctions.contains(json["type"].get<std::string>()))
                 throw Error::InvalidPrefabFileError();
+            _prefabs[name].emplace(_componentFunctions[json["type"].get<std::string>()](json.at("value")));
         }
     }
 } // namespace GameEngine
