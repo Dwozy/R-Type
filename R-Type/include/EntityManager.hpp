d4d85be6285a90aa9c42995e659e2448b4b1b77f
Leo Soidiki <leo.soidiki@epitech.eu>
2023-10-23 17:25:35 +0200

    feat: create class for input

--- R-Type/include/EntityManager.hpp
+++ R-Type/include/EntityManager.hpp
@@ -31,7 +31,6 @@ namespace GameEngine
         /// @param entity that will be attribute the controller component
         /// @param registry game engine registry that will set the components
         void setControlPlayerEntity(Entity entity, Registry &registry);
-
       protected:
       private:
     };
