8a6f53566a309acfc49767992478e1d992c9bfb4
Axel Humeau <axel.humeau@epitech.eu>
2023-11-03 16:57:08 +0100

    ci: add new artifact building to windows script

--- build_artifact.sh
+++ build_artifact.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-# ./build_unix.sh
-
-# mkdir -p ./artifacts/client/R-Type
-# mkdir -p ./artifacts/server
-
-# cp -r R-Type/assets ./artifacts/client/R-Type
-
-# cp R-Type/r-type_client ./artifacts/client
-# cp R-Type-server/r-type_server ./artifacts/server
-
-# echo '#!/bin/bash
-
-# IFS="/" read -r -a array <<< "$SHELL"
-# shell=${array[${#array[@]}-1]}
-
-# echo "export PATH=\"$(pwd):$PATH\"" >> ~/."$shell"rc
-# ' > ./artifacts/server/install.sh
-
-# chmod +x ./artifacts/server/install.sh
-# cp ./artifacts/server/install.sh ./artifacts/client
-
-# find . -name "*.so" -exec cp {} ./artifacts/client \;
-# find . -name "*.so" -exec cp {} ./artifacts/server \;
-
-# zip artifacts/r-type_client_linux_x86-64.zip ./artifacts/client/*
-# zip artifacts/r-type_server_linux_x86-64.zip ./artifacts/server/*
-
 version="$1"
 
 echo "-- Cleaning build folder"
@@ -36,7 +9,7 @@ echo "-- Cleaning build folder - done"
 mkdir artifacts
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_CLIENT="" -DINSTALL=""
-cd build && sudo cpack --config CPackConfig.cmake -G ZIP && sudo cpack --config CPackConfig.cmake -G STGZ
+cd build && cpack --config CPackConfig.cmake -G ZIP && cpack --config CPackConfig.cmake -G STGZ
 status=$?
 if [ "$status" -ne 0 ]; then
     exit "$status"
@@ -47,7 +20,7 @@ cd -
 rm -rf build/CMakeCache.txt
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SERVER="" -DINSTALL=""
-cd build && sudo cpack --config CPackConfig.cmake -G ZIP && sudo cpack --config CPackConfig.cmake -G STGZ
+cd build && cpack --config CPackConfig.cmake -G ZIP && cpack --config CPackConfig.cmake -G STGZ
 status=$?
 if [ "$status" -ne 0 ]; then
     exit "$status"
@@ -58,7 +31,7 @@ cd -
 rm -rf build/CMakeCache.txt
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_GAME_PLATFORMER="" -DINSTALL=""
-cd build && sudo cpack --config CPackConfig.cmake -G ZIP && sudo cpack --config CPackConfig.cmake -G STGZ
+cd build && cpack --config CPackConfig.cmake -G ZIP && cpack --config CPackConfig.cmake -G STGZ
 status=$?
 if [ "$status" -ne 0 ]; then
     exit "$status"
@@ -69,7 +42,7 @@ cd -
 rm -rf build/CMakeCache.txt
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_GAME_ENGINE="" -DINSTALL=""
-cd build && sudo cpack --config CPackConfig.cmake -G ZIP && sudo cpack --config CPackConfig.cmake -G STGZ
+cd build && cpack --config CPackConfig.cmake -G ZIP && cpack --config CPackConfig.cmake -G STGZ
 status=$?
 if [ "$status" -ne 0 ]; then
     exit "$status"
