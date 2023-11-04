d6254c5e8d8ecbfc5d51eb0126b19d1a9c57efaf
Timothé <timothe.zheng@epitech.eu>
2023-11-04 17:19:25 +0100

    fix(immunity): remove function handling imunnity split

--- R-Type-server/src/Game/Immunity.cpp
+++ R-Type-server/src/Game/Immunity.cpp
@@ -9,23 +9,6 @@
 
 namespace RType::Server
 {
-    void RType::Server::RTypeServer::setImmunity(std::chrono::duration<float> timerInvincibility,
-        std::pair<const uint16_t, std::pair<bool, std::chrono::steady_clock::time_point>> playerTimer,
-        std::chrono::steady_clock::time_point &now)
-    {
-        if (timerInvincibility.count() > 3.0) {
-            playerTimer.second.second = now;
-            playerTimer.second.first = true;
-            struct RType::Protocol::StatePlayerData statePlayer = {
-                .id = playerTimer.first, .invincibility = static_cast<uint8_t>(false)};
-            std::vector<std::byte> dataToSend =
-                Serialization::serializeData<struct RType::Protocol::StatePlayerData>(statePlayer, sizeof(statePlayer));
-            for (auto client : _udpServer.getListClients())
-                _udpServer.sendInformation(
-                    static_cast<uint8_t>(RType::Protocol::ComponentType::TEXTURE_STATE), dataToSend, client.second);
-        }
-    }
-
     void RType::Server::RTypeServer::handleImmunity(std::chrono::steady_clock::time_point &now)
     {
         for (auto &playerTimer : _timerLifePoint) {
@@ -33,7 +16,18 @@ namespace RType::Server
                 continue;
             std::chrono::duration<float> _deltaTimerInvicibility =
                 std::chrono::duration_cast<std::chrono::duration<float>>(now - playerTimer.second.second);
-            setImmunity(_deltaTimerInvicibility, playerTimer, now);
+            if (_deltaTimerInvicibility.count() > 3.0) {
+                playerTimer.second.second = now;
+                playerTimer.second.first = true;
+                struct RType::Protocol::StatePlayerData statePlayer = {
+                    .id = playerTimer.first, .invincibility = static_cast<uint8_t>(false)};
+                std::vector<std::byte> dataToSend =
+                    Serialization::serializeData<struct RType::Protocol::StatePlayerData>(
+                        statePlayer, sizeof(statePlayer));
+                for (auto client : _udpServer.getListClients())
+                    _udpServer.sendInformation(
+                        static_cast<uint8_t>(RType::Protocol::ComponentType::TEXTURE_STATE), dataToSend, client.second);
+            }
         }
     }
 
